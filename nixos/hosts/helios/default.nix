e37380eaa4fffa9213b38ebfe079fad493b1ac21
cullvox <cullvox@outlook.com>
2023-09-17 14:31:08 -0400

    Adding proper nas info for mounting.

--- nixos/hosts/helios/default.nix
+++ nixos/hosts/helios/default.nix
@@ -35,7 +35,7 @@
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,vers=1.0";
     in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
   };
 
