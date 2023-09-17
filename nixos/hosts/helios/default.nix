1dafc2391d2de863b13602d9f2ac09265923c0dc
cullvox <cullvox@outlook.com>
2023-09-17 14:14:24 -0400

    add mount nas.

--- nixos/hosts/helios/default.nix
+++ nixos/hosts/helios/default.nix
@@ -15,6 +15,7 @@
     package = pkgs.nextcloud27;
     # hostName = "cadenmiller.dev";
     hostName = "192.168.1.32";
+    home = "/mnt/cloud";
     config.adminpassFile = "${pkgs.writeText "adminpass" "test123"}";
     # https = true;
     configureRedis = true;
@@ -26,6 +27,17 @@
   #};
 
   #security.acme.acceptTerms = true;
+
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [pkgs.cifs-utils];
+  fileSystems."/mnt/cloud" = {
+    device = "//192.168.1.85/Cloud";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
 
   # Open some ports
   networking.firewall.allowedTCPPorts = [80 443 25565 25566]; # mc, mc bta, https:mailzor
